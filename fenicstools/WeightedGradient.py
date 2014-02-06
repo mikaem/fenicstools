24af0f423c253a4d5cbd118aafaac07cfb246de5
Mikael Mortensen <mikaem@math.uio.no>
2014-02-06 09:42:55 -0800

    Restructuring and shaping up for release

--- fenicstools/WeightedGradient.py
+++ fenicstools/WeightedGradient.py
@@ -3,7 +3,7 @@ __date__ = "2013-12-13"
 __copyright__ = "Copyright (C) 2013 " + __author__
 __license__  = "GNU Lesser GPL version 3 or any later version"
 import os, inspect
-from dolfin import *
+from dolfin import compile_extension_module, Function, FunctionSpace, assemble, TrialFunction, TestFunction, dx, Matrix, 
 
 fem_folder = os.path.abspath(os.path.join(inspect.getfile(inspect.currentframe()), "../fem"))
 gradient_code = open(os.path.join(fem_folder, 'gradient_weight.cpp'), 'r').read()
@@ -42,17 +42,3 @@ def weighted_gradient_matrix(mesh, i, degree=1, constrained_domain=None):
         dP = assemble(TrialFunction(CG1).dx(i)*TestFunction(DG)*dx)
         compiled_gradient_module.compute_weighted_gradient_matrix(G, dP, C, dg)
         return C
-
-if __name__ == '__main__':
-    mesh = UnitSquareMesh(10, 10)
-    V = FunctionSpace(mesh, 'CG', 1)
-    u = interpolate(Expression("x[0]+2*x[1]"), V)
-    wx = weighted_gradient_matrix(mesh, 0)
-    dux = Function(V)
-    dux.vector()[:] = wx * u.vector()
-    wy = weighted_gradient_matrix(mesh, 1)
-    duy = Function(V)
-    duy.vector()[:] = wy * u.vector()
-    plot(dux, title='One')
-    plot(duy, title='Two')
-    
