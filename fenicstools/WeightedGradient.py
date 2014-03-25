598112b4e8e68d8012b2f520f49b7db806570701
mirok <miroslav.kuchta@gmail.com>
2014-03-25 22:20:44 +0100

    dding CR to weighted gradient computations

--- fenicstools/WeightedGradient.py
+++ fenicstools/WeightedGradient.py
@@ -9,35 +9,57 @@ fem_folder = os.path.abspath(os.path.join(inspect.getfile(inspect.currentframe()
 gradient_code = open(os.path.join(fem_folder, 'gradient_weight.cpp'), 'r').read()
 compiled_gradient_module = compile_extension_module(code=gradient_code)
 
-def weighted_gradient_matrix(mesh, i, degree=1, constrained_domain=None):
-    """Compute weighted gradient matrix
+def weighted_gradient_matrix(mesh, i, family='CG', degree=1, constrained_domain=None):
+  """Compute weighted gradient matrix
+  
+  The matrix allows you to compute the gradient of a P1 Function 
+  through a simple matrix vector product
+  
+  CG family:
+    p_ is the pressure solution on CG1
+    dPdX = weighted_gradient_matrix(mesh, 0, degree)
+    V = FunctionSpace(mesh, 'CG', degree)
+    dpdx = Function(V)
+    dpdx.vector()[:] = dPdX * p_.vector()
     
-    The matrix allows you to compute the gradient of a P1 Function 
-    through a simple matrix vector product
-    
-      p_ is the pressure solution on CG1
-      dPdX = weighted_gradient_matrix(mesh, 0, degree)
-      V = FunctionSpace(mesh, 'CG', degree)
-      dpdx = Function(V)
-      dpdx.vector()[:] = dPdX * p_.vector()
-      
     The space for dpdx must be continuous Lagrange of some order
-      
-    """
-    DG = FunctionSpace(mesh, 'DG', 0)
-    CG = FunctionSpace(mesh, 'CG', degree, constrained_domain=constrained_domain)
-    CG1 = FunctionSpace(mesh, 'CG', 1, constrained_domain=constrained_domain)
-    G = assemble(TrialFunction(DG)*TestFunction(CG)*dx)
-    dg = Function(DG)
-    if isinstance(i, (tuple, list)):
-        CC = []
-        for ii in i:
-            dP = assemble(TrialFunction(CG1).dx(ii)*TestFunction(DG)*dx)
-            A = Matrix(G)
-            Cp = compiled_gradient_module.compute_weighted_gradient_matrix(A, dP, dg)
-            CC.append(Cp)
-        return CC
-    else:
-        dP = assemble(TrialFunction(CG1).dx(i)*TestFunction(DG)*dx)
-        Cp = compiled_gradient_module.compute_weighted_gradient_matrix(G, dP, dg)
-        return Cp
+  
+  CR family:
+    p_ is the pressure solution on CR
+    dPdX = weighted_gradient_matrix(mesh, 0, 1)
+    V = FunctionSpace(mesh, 'CR', 1)
+    dpdx = Function(V)
+    dpdx.vector()[:] = dPdX * p_.vector()
+    
+  """
+  
+  DG = FunctionSpace(mesh, 'DG', 0)
+  
+  if family == 'CG':
+    # Source and Target spaces are CG_1 and CG_degree
+    S = FunctionSpace(mesh, 'CG', 1, constrained_domain=constrained_domain)
+    T = FunctionSpace(mesh, 'CG', degree, constrained_domain=constrained_domain)
+  elif family == 'CR':
+    if degree != 1:
+        print '\033[1;37;34m%s\033[0m' % 'Ignoring degree'
+
+    # Source and Target spaces are CR
+    S = FunctionSpace(mesh, 'CR', 1, constrained_domain=constrained_domain)
+    T = S
+  else:
+    raise ValueError('Only CG and CR families are allowed.')
+    
+  G = assemble(TrialFunction(DG)*TestFunction(T)*dx)
+  dg = Function(DG)
+  if isinstance(i, (tuple, list)):
+    CC = []
+    for ii in i:
+      dP = assemble(TrialFunction(S).dx(ii)*TestFunction(DG)*dx)
+      A = Matrix(G)
+      Cp = compiled_gradient_module.compute_weighted_gradient_matrix(A, dP, dg)
+      CC.append(Cp)
+    return CC
+  else:
+    dP = assemble(TrialFunction(S).dx(i)*TestFunction(DG)*dx)
+    Cp = compiled_gradient_module.compute_weighted_gradient_matrix(G, dP, dg)
+    return Cp
