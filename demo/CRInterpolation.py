60afeb22a07969448cfb5b68ecdc5a79a000d152
Mikael Mortensen <mikaem@math.uio.no>
2014-04-07 07:11:39 -0700

    Adding interpolation demo

---
+++ demo/CRInterpolation.py
@@ -0,0 +1,12 @@
+from dolfin import *
+
+mesh = UnitSquareMesh(100, 100)
+CG = VectorFunctionSpace(mesh, 'CG', 1)
+u = interpolate(Expression(("sin(2*pi*x[0])", "cos(3*pi*x[1])")), CG)
+
+C = divergence_matrix(u)
+DG = FunctionSpace(mesh, 'DG', 0)
+cc = Function(DG)
+cc.vector()[:] = C * u.vector()
+plot(cc, title='Gauss div')
+plot(div(u), title="Projection")
