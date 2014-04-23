fb6ee4017670e7840aee2290c09ba7e2dc16748b
mirok <miroslav.kuchta@gmail.com>
2014-04-23 11:51:36 +0200

    dofmapplotter alpha version

--- fenicstools/__init__.py
+++ fenicstools/__init__.py
@@ -5,3 +5,4 @@ from Interpolation import interpolate_nonmatching_mesh
 from common import getMemoryUsage, SetMatrixValue
 from Streamfunctions import StreamFunction, StreamFunction3D
 from GaussDivergence import gauss_divergence, divergence_matrix
+from DofMapPlotter import DofMapPlotter
---
+++ fenicstools/dofmapplotter/__init__.py
@@ -0,0 +1,2 @@
+from common import extract_dofmaps, bounds, is_function_space
+from dofmapplot import DofMapPlot
