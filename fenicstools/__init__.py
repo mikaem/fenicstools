6b323df20bc1c25f8d59682393cf90dc7e50186c
= <=>
2014-08-08 18:45:04 +0200

    Removing interpolation nonmatching mesh because this is now available in dolfin

--- fenicstools/__init__.py
+++ fenicstools/__init__.py
@@ -1,8 +1,10 @@
 from Probe import Probe, Probes, StatisticsProbe, StatisticsProbes
 from StructuredGrid import StructuredGrid, ChannelGrid
 from WeightedGradient import weighted_gradient_matrix, compiled_gradient_module
-from Interpolation import interpolate_nonmatching_mesh
 from common import getMemoryUsage, SetMatrixValue
 from Streamfunctions import StreamFunction, StreamFunction3D
 from GaussDivergence import gauss_divergence, divergence_matrix
-from DofMapPlotter import DofMapPlotter
+try:
+    from DofMapPlotter import DofMapPlotter
+except:
+    pass # Probably missing dependency
