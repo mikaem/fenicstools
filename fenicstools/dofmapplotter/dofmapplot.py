84598683b92e4e4fdb8609832142f1790eae1765
Mikael Mortensen <mikaem@math.uio.no>
2016-03-01 09:38:33 +0100

    Trying to get parallel h5py in travis

--- fenicstools/dofmapplotter/dofmapplot.py
+++ fenicstools/dofmapplotter/dofmapplot.py
@@ -3,7 +3,6 @@ __date__ = '2014-04-23'
 __copyright__ = 'Copyright (C) 2013 ' + __author__
 __license__ = 'GNU Lesser GPL version 3 or any later version'
 
-from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 from meshentityhandler import MeshEntityHandler
 from dofhandler import DofHandler
