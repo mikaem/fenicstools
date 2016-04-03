46a899f3c9a466768241e02a08e7cc9a04bb64a7
Miroslav Kuchta <miroslav.kuchta@gmail.com>
2016-04-03 22:19:34 +0200

    as class

--- fenicstools/ClementInterpolation.py
+++ fenicstools/ClementInterpolation.py
@@ -9,13 +9,14 @@ RED = '\033[1;37;31m%s\033[0m'
 
 class TimerDecorator(object):
     '''Use dolfin.Timer to get execution time.'''
-    def __init__(self, name):
+    def __init__(self, name, depth=1):
         self.name = GREEN % name
+        self.depth = depth
     def __call__(self, f):
         def wrapped_f(*args, **kwargs):
             timer = Timer(self.name)
             ans = f(*args, **kwargs)
-            info(' '.join(['\t', self.name, 'done in %.2f s.' % timer.stop()]))
+            info(' '.join(['\t'*self.depth, self.name, 'done in %.2f s.' % timer.stop()]))
             return ans
         return wrapped_f
 
@@ -32,50 +33,80 @@ class ClementInterpolant(object):
         2) Set Ih(expr) = sum_j Qj(expr)vj.
     '''
 
+    @TimerDecorator('precompute Clement interpolant')
     def __init__(self, expr):
         '''For efficient interpolation things are precomuputed here'''
         # Analyze expr and raise if invalid
-        # Analyze shape and raise if expr cannot be represented
-        # Extract mesh and raise if it is not unique or missing
-        # Precompute averaging operator
-        # Precompute 'mass matrix inverse'
-        # Compute the forms
-        pass
-
-    def __call__(self):
-        '''Return the interpolant'''
-        pass
-
-
-def clement_interpolate(expr):
-    '''Construct the Clement interpolant of expr.'''
-    # Make sure that the expression is valid
-    try:
         terminals = analyze_expr(expr)
-    except ValueError as e:
-        print e
-        return None
-
-    # Now the shape can be extracted
-    shape = expr.ufl_shape
-    try:
+        # Analyze shape and raise if expr cannot be represented
+        shape = expr.ufl_shape
         analyze_shape(shape)
-    except ValueError as e:
-        print e
-        return None
-
-    # Extract the unique mesh from expr operands
-    try:
+        # Extract mesh from expr operands and raise if it is not unique or missing
         mesh = extract_mesh(terminals)
-    except ValueError as e:
-        print e
-        return None
+        # Compute things for constructing Q
+        Q = FunctionSpace(mesh, 'DG', 0)
+        q = TestFunction(Q)
+        # Need cell volumes for averaging [lhs]
+        volumes = assemble(inner(Constant(1), q)*dx)
+        # L2 projections [rhs]
+        # Scalar, Vectors, Tensors are built from components
+        # Translate expression into forms for individual components
+        if len(shape) == 0: forms = [inner(expr, q)*dx]
+        elif len(shape) == 1: forms = [inner(expr[i], q)*dx for i in range(shape[0])]
+        else: forms = [inner(expr[i, j], q)*dx for i in range(shape[0]) for j in range(shape[1])]
+        # L2 projections of comps to indiv. cells
+        projections = map(assemble, forms)
+        # Precompute averaging operator: Interpolant will be built from entries of 
+        # projections/volumes in appropriate cells of the patch that supports basis 
+        # functions of CG_1. Map of the entries to single dof value is provided by 
+        # averaging operator A
+        V = FunctionSpace(mesh, 'CG', 1)
+        A = construct_averaging_operator(V)
+        # Precompute 'mass matrix inverse'
+        patch_volumes = Function(V).vector()
+        A.mult(volumes, patch_volumes)
+        # Awkard poitwise inverse (inverting the mass matrix)
+        patch_volumes = as_backend_type(patch_volumes)
+        try:
+            patch_volumes.vec()[:] = 1./patch_volumes.vec()
+        except AttributeError:
+            patch_volumes.set_local(1./patch_volumes.get_local())
+        patch_volumes.apply('insert')
+        # Collect stuff
+        self.shape, self.V, self.A, self.patch_volumes, self.projections = \
+                shape, V, A, patch_volumes, projections
 
-    # Construct the interpolant
-    return construct_clement_interpolant(expr, shape, mesh)
 
-# --- Implementation
-@TimerDecorator('analyze expression')
+    @TimerDecorator('compute Clement interpolant')
+    def __call__(self):
+        '''Return the interpolant.'''
+        shape, V, A, patch_volumes, projections = \
+                self.shape, self.V, self.A, self.patch_volumes, self.projections
+        # The interpolant (scalar, vector, tensor) is build from components
+        components = []
+        for projection in projections:
+            component = Function(V)
+            # Compute rhs for L2 patch projection
+            A.mult(projection, component.vector()) 
+            # Apply the mass matrix inverse
+            component.vector()[:] *= patch_volumes
+            components.append(component)
+        # Finalize the interpolant
+        # Scalar has same space as component
+        if len(shape) == 0: 
+            uh = components.pop()
+        # Other ranks
+        else:
+            W = VectorFunctionSpace(mesh, 'CG', 1, dim=shape[0]) if len(shape) == 1 else\
+                TensorFunctionSpace(mesh, 'CG', 1, shape=shape)
+            uh = Function(W)
+            assign(uh, components)
+
+        return uh
+
+# Workers--
+
+@TimerDecorator('analyze expression', 2)
 def analyze_expr(expr):
     '''
     A valid expr for Clement interpolation is defined only in terms of pointwise
@@ -91,7 +122,7 @@ def analyze_expr(expr):
     return terminals
 
 
-@TimerDecorator('analyze shape')
+@TimerDecorator('analyze shape', 2)
 def analyze_shape(shape):
     '''
     The shape of expr that UFL can build is arbitrary but we only support
@@ -102,7 +133,7 @@ def analyze_shape(shape):
         raise ValueError('Interpolating Expr does not result rank-0, 1, 2 function')
 
 
-@TimerDecorator('extract mesh')
+@TimerDecorator('extract mesh', 2)
 def extract_mesh(terminals):
     '''Get the common mesh of operands that make the expression.'''
     pairs = []
@@ -118,69 +149,8 @@ def extract_mesh(terminals):
     # Mesh of Nones of multiple
     raise ValueError('Failed to extract mesh: Operands with no or different meshes')
 
-@TimerDecorator('construct Clement interpolant')
-def construct_clement_interpolant(expr, shape, mesh):
-    '''
-    Here, the Clement interpolant is a CG_1 function over msh constructed in 
-    two steps (See Braess' book):
-        1) For each mesh vertex xj let wj the union of cells that share the vertex 
-           (i.e wj is the support of vj - the basis function of CG_1 function
-           space such that vj(xj) = 1). Then Qj(expr) is an L2 projection of
-           expr into constant field on wj.
-        2) Set Ih(expr) = sum_j Qj(expr)vj.
-    '''
-    # 1: L2 Projections
-    Q = FunctionSpace(mesh, 'DG', 0)
-    # Scalar, Vectors, Tensors are built from components
-    q = TestFunction(Q)
-    volumes = assemble(inner(Constant(1), q)*dx)
-    # Translate expression into forms for individual components
-    if len(shape) == 0: forms = [inner(expr, q)*dx]
-    elif len(shape) == 1: forms = [inner(expr[i], q)*dx for i in range(shape[0])]
-    else: forms = [inner(expr[i, j], q)*dx for i in range(shape[0]) for j in range(shape[1])]
-    # L2 projections of comps to indiv. cells
-    projections = map(assemble, forms)
-    # Interpolant will be built from entries of projections/volumes in appropriate 
-    # cells of the patch that supports basis functions of CG_1. Map of the
-    # entries to single dof value is provided by averaging operator A
-    V = FunctionSpace(mesh, 'CG', 1)
-    A = construct_averaging_operator(V)
 
-    # Map to CG1. Volumes only once
-    patch_volumes = Function(V).vector()
-    A.mult(volumes, patch_volumes)
-    # Awkard poitwise inverse (inverting the mass matrix)
-    patch_volumes = as_backend_type(patch_volumes)
-    try:
-        patch_volumes.vec()[:] = 1./patch_volumes.vec()
-    except AttributeError:
-        patch_volumes.set_local(1./patch_volumes.get_local())
-    patch_volumes.apply('insert')
-
-    # Now the components
-    components = []
-    for projection in projections:
-        component = Function(V)
-        # Compute rhs for L2 patch projection
-        A.mult(projection, component.vector()) 
-        # Apply the mass matrix inverse
-        component.vector()[:] *= patch_volumes
-        components.append(component)
-    # Finalize the interpolant
-    # Scalar has same space as component
-    if len(shape) == 0: 
-        uh = components.pop()
-    # Other ranks
-    else:
-        W = VectorFunctionSpace(mesh, 'CG', 1, dim=shape[0]) if len(shape) == 1 else\
-            TensorFunctionSpace(mesh, 'CG', 1, shape=shape)
-        uh = Function(W)
-        assign(uh, components)
-
-    return uh
-
-
-@TimerDecorator('construct averaging operator')
+@TimerDecorator('construct averaging operator', 2)
 def construct_averaging_operator(V):
     '''
     Avaraging matrix has the following properties: It is a map from DG0 to CG1.
@@ -207,6 +177,16 @@ def construct_averaging_operator(V):
     A = assemble((1./K)*Constant(tdim+1)*inner(v, q)*dX)
 
     return A
+
+def clement_interpolate(expr):
+    '''
+    A free function for construting Clement interpolant of an expr. This is
+    done by creating instance of ClementInterpolant and applying it. The
+    instance is not cached. The function is intended for one time interpolation.
+    '''
+    ci = ClementInterpolant(expr)
+    return ci()
+
 
 # --- Tests
 
