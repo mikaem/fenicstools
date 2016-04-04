76db186536a51c380e2d7d7f5ec804e21eebff4d
Miroslav Kuchta <miroslav.kuchta@gmail.com>
2016-04-04 22:08:59 +0200

    see if parallel test passes

--- fenicstools/ClementInterpolation.py
+++ fenicstools/ClementInterpolation.py
@@ -2,22 +2,6 @@ from dolfin import *
 import ufl
 
 
-class TimerDecorator(object):
-    '''Use dolfin.Timer to get execution time.'''
-    def __init__(self, name, depth=1, verbose=True):
-        self.name = '\033[1;37;32m%s\033[0m' % name
-        self.depth = depth
-        self.verbose = verbose
-    def __call__(self, f):
-        def wrapped_f(*args, **kwargs):
-            timer = Timer(self.name)
-            ans = f(*args, **kwargs)
-            if self.verbose:
-                info(' '.join(['\t'*self.depth, self.name, 'done in %.2f s.' % timer.stop()]))
-            return ans
-        return wrapped_f
-
-
 class ClementInterpolant(object):
     '''
     This class implements efficient construction of Clement interpolant of an
@@ -29,56 +13,62 @@ class ClementInterpolant(object):
            expr into constant field on wj.
         2) Set Ih(expr) = sum_j Qj(expr)vj.
     '''
+    # Cannot interpolate expression with Arguments + things which are not well
+    # defined at vertex
+    black_listed = (ufl.Argument, ufl.MaxCellEdgeLength, ufl.MaxFacetEdgeLength,
+                    ufl.MinCellEdgeLength, ufl.MinFacetEdgeLength,
+                    ufl.FacetArea, ufl.FacetNormal, 
+                    ufl.CellNormal, ufl.CellVolume)
 
-    @TimerDecorator('precompute Clement interpolant')
     def __init__(self, expr):
         '''For efficient interpolation things are precomuputed here'''
         # Analyze expr and raise if invalid
         terminals = _analyze_expr(expr)
         # Analyze shape and raise if expr cannot be represented
+        _analyze_shape(expr.ufl_shape)
         shape = expr.ufl_shape
-        _analyze_shape(shape)
         # Extract mesh from expr operands and raise if it is not unique or missing
         mesh = _extract_mesh(terminals)
         # Compute things for constructing Q
         Q = FunctionSpace(mesh, 'DG', 0)
         q = TestFunction(Q)
-        # Need cell volumes for averaging [lhs]
-        volumes = assemble(inner(Constant(1), q)*dx)
         # L2 projections [rhs]
         # Scalar, Vectors, Tensors are built from components
         # Translate expression into forms for individual components
         if len(shape) == 0: forms = [inner(expr, q)*dx]
         elif len(shape) == 1: forms = [inner(expr[i], q)*dx for i in range(shape[0])]
         else: forms = [inner(expr[i, j], q)*dx for i in range(shape[0]) for j in range(shape[1])]
-        # L2 projections of comps to indiv. cells
-        projections = map(assemble, forms)
         # Precompute averaging operator: Interpolant will be built from entries of 
         # projections/volumes in appropriate cells of the patch that supports basis 
         # functions of CG_1. Map of the entries to single dof value is provided by 
         # averaging operator A
         V = FunctionSpace(mesh, 'CG', 1)
         A = _construct_averaging_operator(V)
-        # Precompute 'mass matrix inverse'
+        # Precompute 'mass matrix inverse [lhs]
+        volumes = assemble(inner(Constant(1), q)*dx)
         patch_volumes = Function(V).vector()
         A.mult(volumes, patch_volumes)
-        # Awkard poitwise inverse (inverting the mass matrix)
+
         patch_volumes = as_backend_type(patch_volumes)
         try:
-            patch_volumes.vec()[:] = 1./patch_volumes.vec()
+            patch_volumes.vec().reciprocal()
+        # Awkard poitwise inverse (inverting the mass matrix) in case the
+        # backend is not PETSc which has the nice reciprocal function
         except AttributeError:
             patch_volumes.set_local(1./patch_volumes.get_local())
-        patch_volumes.apply('insert')
+            patch_volumes.apply('insert')
+
         # Collect stuff
-        self.shape, self.V, self.A, self.patch_volumes, self.projections = \
-                shape, V, A, patch_volumes, projections
+        self.shape, self.V, self.A, self.patch_volumes, self.forms = \
+                shape, V, A, patch_volumes, forms
 
-
-    @TimerDecorator('compute Clement interpolant')
     def __call__(self):
         '''Return the interpolant.'''
-        shape, V, A, patch_volumes, projections = \
-                self.shape, self.V, self.A, self.patch_volumes, self.projections
+        shape, V, A, patch_volumes, forms = \
+                self.shape, self.V, self.A, self.patch_volumes, self.forms
+
+        # L2 projections of comps to indiv. cells
+        projections = map(assemble, forms)
         # The interpolant (scalar, vector, tensor) is build from components
         components = []
         for projection in projections:
@@ -86,7 +76,7 @@ class ClementInterpolant(object):
             # Compute rhs for L2 patch projection
             A.mult(projection, component.vector()) 
             # Apply the mass matrix inverse
-            component.vector()[:] *= patch_volumes
+            component.vector()[:] *= patch_volumes  # hould apply insert
             components.append(component)
         # Finalize the interpolant
         # Scalar has same space as component
@@ -99,12 +89,15 @@ class ClementInterpolant(object):
                 TensorFunctionSpace(mesh, 'CG', 1, shape=shape)
             uh = Function(W)
             assign(uh, components)
+            # NOTE: assign might not use apply correctly. see 
+            # https://bitbucket.org/fenics-project/dolfin/issues/587/functionassigner-does-not-always-call
+            # So just to be sure
+            uh.vector().apply('insert')
 
         return uh
 
 # Workers--
 
-@TimerDecorator('analyze expression', 2)
 def _analyze_expr(expr):
     '''
     A valid expr for Clement interpolation is defined only in terms of pointwise
@@ -114,13 +107,12 @@ def _analyze_expr(expr):
     if isinstance(expr, ufl.Form): raise ValueError('Expression is a form')
     # Elliminate expressions build from Trial/Test functions, FacetNormals 
     terminals = [t for t in ufl.algorithms.traverse_unique_terminals(expr)]
-    if any(isinstance(t, (ufl.Argument, ufl.FacetNormal)) for t in terminals):
+    if any(isinstance(t, ClementInterpolant.black_listed) for t in terminals):
         raise ValueError('Invalid expression (e.g. has Arguments as operand)')
     # At this point the expression is valid
     return terminals
 
 
-@TimerDecorator('analyze shape', 2)
 def _analyze_shape(shape):
     '''
     The shape of expr that UFL can build is arbitrary but we only support
@@ -131,7 +123,6 @@ def _analyze_shape(shape):
         raise ValueError('Interpolating Expr does not result rank-0, 1, 2 function')
 
 
-@TimerDecorator('extract mesh', 2)
 def _extract_mesh(terminals):
     '''Get the common mesh of operands that make the expression.'''
     pairs = []
@@ -148,7 +139,6 @@ def _extract_mesh(terminals):
     raise ValueError('Failed to extract mesh: Operands with no or different meshes')
 
 
-@TimerDecorator('construct averaging operator', 2)
 def _construct_averaging_operator(V):
     '''
     Avaraging matrix has the following properties: It is a map from DG0 to CG1.
