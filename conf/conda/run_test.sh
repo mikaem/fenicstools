9f27d3fece8e0b978f92d81c47be89b4a91d0eb8
Mikael Mortensen <mikaem@math.uio.no>
2016-12-13 11:56:06 +0100

    Setting INSTANT_CACHE_DIR in build.sh

--- conf/conda/run_test.sh
+++ conf/conda/run_test.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -e
 
-if [[ "$(uname)" == "Darwin" ]]; then
-  export MACOSX_DEPLOYMENT_TARGET=10.9
-  export CXXFLAGS="-std=c++11 -stdlib=libc++ $CXXFLAGS"
-fi
-
-export INSTANT_CACHE_DIR=${SRC_DIR}/instant
-
 pushd "$SRC_DIR/tests"
 python -b -m pytest -vs
 
