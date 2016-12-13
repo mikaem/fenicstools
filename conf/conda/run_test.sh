16a7aed6f5b5333d0ec8e1f7f60a77f6789e5085
Mikael Mortensen <mikaem@math.uio.no>
2016-12-13 12:10:27 +0100

    Fixing .travis psx build

--- conf/conda/run_test.sh
+++ conf/conda/run_test.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+if [[ "$(uname)" == "Darwin" ]]; then
+  export MACOSX_DEPLOYMENT_TARGET=10.9
+  export CXXFLAGS="-std=c++11 -stdlib=libc++ $CXXFLAGS"
+fi
+
+export INSTATS_CACHE_DIR="${SRC_DIR}/instant"
+
 pushd "$SRC_DIR/tests"
 python -b -m pytest -vs
 
