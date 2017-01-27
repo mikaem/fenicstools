7ec53eba06b30d448cf08a26384ac028ed733fd4
miro3 <miroslav.kuchta@gmail.com>
2017-01-27 17:43:07 +0100

    fix cells(cwp) == empty-iterator; now there is patch of cells connected to cwp by vertices

--- fenicstools/LagrangianParticles.py
+++ fenicstools/LagrangianParticles.py
@@ -48,6 +48,12 @@ class CellWithParticles(df.Cell):
         # Make an empty list of particles that I carry
         self.particles = []
         self += particle
+        # Make the cell aware of its neighbors; neighbor cells are cells
+        # connected to this one by vertices
+        tdim = mesh.topology().dim()
+        self.neighbors = map(lambda neighbor_index: df.Cell(mesh, neighbor_index), 
+                             sum((vertex.entities(tdim).tolist()
+                                  for vertex in df.vertices(self)), []))
 
     def __add__(self, particle):
         'Add single particle to cell.'
@@ -108,7 +114,6 @@ class LagrangianParticles:
 
         self.V = V
         self.mesh = V.mesh()
-        self.mesh.init(2, 2)  # Cell-cell connectivity for neighbors of cell
         self.tree = self.mesh.bounding_box_tree()  # Tree for isection comput.
 
         # Allocate some variables used to look up the velocity
@@ -119,6 +124,7 @@ class LagrangianParticles:
         # update basis_i(x) depending on x, i.e. particle where we make
         # interpolation. This updaea mounts to computing the basis matrix
         self.dim = self.mesh.topology().dim()
+        self.mesh.init(0, self.dim)  # Vertex-to-cell connectivity for neighb. comput
 
         self.element = V.dolfin_element()
         self.num_tensor_entries = 1
@@ -241,7 +247,7 @@ class LagrangianParticles:
                 if not cwp.contains(point):
                     found = False
                     # Check neighbor cells
-                    for neighbor in df.cells(cwp):
+                    for neighbor in cwp.neighbors:
                         if neighbor.contains(point):
                             new_cell_id = neighbor.index()
                             found = True
